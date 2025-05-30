cf25bf898eb2d2972ea45908bddf4d996f9e9ee4
Stephen Harris <sweharris@users.noreply.github.com>
2025-05-30 09:45:43 -0400

    Add a search command

--- management.go
+++ management.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func rescan() {
@@ -50,3 +51,24 @@ func show_dbinfo() {
 }
 
 var _ bool = register_fn("dbinfo", show_dbinfo, "Song details in MPD database")
+
+// Searches artist/title/album and returns the file for matching entries
+func search_db() {
+	if len(Args) == 0 {
+		die("Missing search string")
+	}
+	// srch_str := strings.ToLower(Args[0])
+	srch_str := strings.ToLower(strings.Join(Args, " "))
+
+	list := mpd_db_info()
+
+	for _, song := range list {
+		if strings.Contains(strings.ToLower(song["Artist"]), srch_str) ||
+			strings.Contains(strings.ToLower(song["Album"]), srch_str) ||
+			strings.Contains(strings.ToLower(song["Title"]), srch_str) {
+			fmt.Println(song["file"])
+		}
+	}
+}
+
+var _ bool = register_fn("search", search_db, "Search for a song")
